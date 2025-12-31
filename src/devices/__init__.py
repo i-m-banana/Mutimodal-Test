323110f07d2c09b65117d7a48ce10196f9af0e0c
i-m-banana <2632430344@qq.com>
2025-12-31 19:59:06 +0800

    update maibobo

--- src/devices/__init__.py
+++ src/devices/__init__.py
@@ -2,7 +2,13 @@
 
 from .base import DeviceBase, DeviceException
 from .eeg import BleEEGDevice, HAS_EEG_HARDWARE
-from .maibobo import MaiboboDevice
+from .maibobo import (
+    MaiboboDevice,
+    HAS_MAIBOBO_DRIVER,
+    DEFAULT_PORT,
+    detect_available_port,
+    parse_frame,
+)
 from .tobii import HAS_TOBII, TobiiDevice
 
 __all__ = [
@@ -11,6 +17,10 @@ __all__ = [
     "BleEEGDevice",
     "HAS_EEG_HARDWARE",
     "MaiboboDevice",
+    "HAS_MAIBOBO_DRIVER",
+    "DEFAULT_PORT",
+    "detect_available_port",
+    "parse_frame",
     "TobiiDevice",
     "HAS_TOBII",
 ]
